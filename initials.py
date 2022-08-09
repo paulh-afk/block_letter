457c95fa595c286851a496b0d77d67b4ba45a4af
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-08-09 21:15:17 +0200

    firstname in block letters

---
+++ initials.py
@@ -0,0 +1,11 @@
+firstname = """
+PPPPP   A   U   U L
+P   P  A A  U   U L
+P   P A   A U   U L
+PPPPP AAAAA U   U L
+P     A   A U   U L
+P     A   A U   U L
+P     A   A  UUU  LLLLL
+"""
+
+print(firstname)
