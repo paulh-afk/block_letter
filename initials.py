92b2a38dbb03c229e6a8d0769b60d0601ccef4f0
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-08-10 21:04:38 +0200

    account name

--- initials.py
+++ initials.py
@@ -1,11 +1,11 @@
-firstname = """
-PPPPP   A   U   U L
-P   P  A A  U   U L
-P   P A   A U   U L
-PPPPP AAAAA U   U L
-P     A   A U   U L
-P     A   A U   U L
-P     A   A  UUU  LLLLL
+account_name = """
+PPPPP   A   U   U L     H    H         A   FFFFF K   K
+P   P  A A  U   U L     H    H        A A  F     K  K
+P   P A   A U   U L     H    H       A   A F     K K
+PPPPP AAAAA U   U L     HHHHHH ----- AAAAA FFFFF KK
+P     A   A U   U L     H    H       A   A F     K K
+P     A   A U   U L     H    H       A   A F     K  K
+P     A   A  UUU  LLLLL H    H       A   A F     K   K
 """
 
-print(firstname)
+print(account_name)
